e0f42d8e4c1c31ce98c42e5809c7bd49c300f4f6
stephan <smuts1989@gmail.com>
2023-07-27 22:57:46 +0200

    Using a custom hook for the userInformation

--- src/Header.js
+++ src/Header.js
@@ -1,45 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './Header.css';
 import { UserContext, CreditContext } from './App';
-import { auth, db } from './Firebase.js';
-import { signOut, onAuthStateChanged } from "firebase/auth";
 import HeaderImage from './static/images/site_logo.png';
 import LoginDialog from './LoginDialog.js';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
 import PricingDialog from './PricingDialog';
-import BuyCreditsDialog from './BuyCreditsDialog';
+import ProfileDialog from './ProfileDialog';
+import useUserSubscription from './useUserSubscription';
 
 function Header() {
   const { user, setUser } = React.useContext(UserContext);
   const [showLoginDialog, setShowLoginDialog] = useState(false);
   const { credits, setCredits } = React.useContext(CreditContext);
-  const plans = [
-    { title: 'Free Plan', price: 'Free', features: ['100 credits once off'] },
-    { title: 'Basic Plan', price: '$20/mo', features: ['500 credits per month'] },
-    { title: 'Pro Plan', price: '$30/mo', features: ['1000 credits per month'] },
-  ];
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      if (currentUser) {
-        const userCreditsRef = doc(db, 'customers', currentUser.uid, 'credits', 'total');
-        getDoc(userCreditsRef).then((docSnap) => {
-          if (docSnap.exists()) {
-            let creditAmount = docSnap.data().amount;
-            creditAmount = creditAmount < 0 ? 0 : creditAmount;
-            setCredits(creditAmount);
-          } else {
-            setDoc(userCreditsRef, { amount: 100 }, { merge: true });
-            setCredits(100);
-          }
-        });
-      }
-    });
-
-    return () => unsubscribe();
-    // eslint-disable-next-line
-  }, [setUser]);
+  const { userPlan, userPlanActivity, renewalDate } = useUserSubscription(setUser, setCredits);
 
   return (
     <header className="header">
@@ -47,14 +19,23 @@ function Header() {
         <div className="logo">
           <img src={HeaderImage} alt="Business Ideas logo" />
         </div>
-        {user && <div className="welcome-div">Welcome, {user.displayName ? `${user.displayName} (${credits} credits)` : `${user.email} (${credits} credits)`}</div>}
+        {user &&
+          <div className="welcome-div">
+            {<>
+              <p>{`Welcome, ${user.displayName ? user.displayName : user.email}`}</p>
+              <p>{`You have ${credits} credits remaining`}</p>
+            </>
+
+            }
+          </div>
+        }
         <div className="HeaderButtonsWrapper">
           {
             user
               ? <>
-                <PricingDialog plans={plans} />
-                <BuyCreditsDialog></BuyCreditsDialog>
-                <button className="transparent-button" onClick={() => signOut(auth)}>Logout</button>
+                <PricingDialog purchaseTypeFilter="recurring" title="Subscriptions"></PricingDialog>
+                <PricingDialog purchaseTypeFilter="one_time" title="Buy Credits"></PricingDialog>
+                <ProfileDialog></ProfileDialog>
               </>
               : <button className="transparent-button" onClick={() => setShowLoginDialog(true)}>Login</button>
           }
